02c77b320c734524be26d8278a06f9d881ced753
Muhammedsadhath <153965479+Muhammedsadhath@users.noreply.github.com>
2025-01-02 06:50:47 +0530

    Update settings.py

--- ecomsad/settings.py
+++ ecomsad/settings.py
@@ -92,16 +92,16 @@ WSGI_APPLICATION = 'ecomsad.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
 
         
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'railway',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'WAAdBUicMFrGPKhdjLirgBnCQgGqgryb',
-        # 'HOST': 'autorack.proxy.rlwy.net',
-        # 'PORT': '17230',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'WAAdBUicMFrGPKhdjLirgBnCQgGqgryb',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '17230',
     
     }
 }
